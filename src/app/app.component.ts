eb0a0cce156839caa3f777d4ef14c0786b8571b2
wang314iu <wang314iu@gmail.com>
2022-09-19 17:40:19 -0400

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'library-test';
+  fg = new FormGroup({
+    tel: new FormControl(),
+    ssn: new FormControl('222'),
+    email: new FormControl('adsf'),
+    date: new FormControl(),
+    dateRange: new FormControl(),
+    textarea: new FormControl(),
+    zipcode: new FormControl('333'),
+    zipcodeExt: new FormControl(),
+    default: new FormControl(),
+    number: new FormControl(3),
+    singleSelect: new FormControl(),
+    multiSelect: new FormControl()
+  });
+
+  maxDate = new Date();
+  minDate = new Date(2020, 12);
+
+  submit() {
+    console.log('>>>>>>>>> formValue', this.fg.value);
+    console.log('>>>>>>>>> errors', this.fg);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CaptorInputModule, CaptorFormModule } from 'components';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatMomentDateModule } from "@angular/material-moment-adapter";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CaptorInputModule,
+    CaptorFormModule,
+    MatMomentDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
